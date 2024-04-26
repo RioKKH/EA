3c6cf7e54bf149777035551a3e391dc48fe9b8a2
Ryoichi <ryoichikakehi@gmail.com>
2024-04-27 00:48:57 +0900

    modified:   experiment.sh

--- onemax/onemaxcpu/experiment.sh
+++ onemax/onemaxcpu/experiment.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 CWD=$(pwd)
+
+RUNALL=15
 FILENAME=onemax_prms.dat
 ORIGINAL=${FILENAME}.org
 GenomeLength=$(seq 32 32 1024)
 PopulationSize=$(seq 32 32 1024)
+DATETIME=$(date +%Y%m%d-%H%M%S)
+readonly RESULTFILE=${CWD}/result_${DATETIME}.csv
 
 CATLOG=${CWD}/cat.dat
 
@@ -15,10 +19,15 @@ fi
 for genome in ${GenomeLength}; do
 	for popsize in ${PopulationSize}; do
 		sed -i "s/^N .*$/N ${genome}/" ${FILENAME}
-		# sed -i "s/^GEN_MAX.*$/GEN_MAX ${genome}/" ${FILENAME}
 		sed -i "s/^POP_SIZE.*$/POP_SIZE ${popsize}/" ${FILENAME}
-		cat ${FILENAME} 2>> ${CATLOG}
-		./onemax >> CPU_${popsize}_${genome}.csv 
-		sleep 0.1
+		for _ in $(seq 1 ${RUNALL}); do
+		#for num in $(seq 1 ${RUNALL}); do
+			cat ${FILENAME} 2>> "${CATLOG}"
+			./onemax >> "${RESULTFILE}"
+			#./onemax >> "CPU_${popsize}_${genome}_${num}.csv"
+		done
 	done
 done
+
+# code backup
+# sed -i "s/^GEN_MAX.*$/GEN_MAX ${genome}/" ${FILENAME}
